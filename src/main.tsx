d4a4e65b3165b14b252a2a679262578b4b8a562f
Youcef <ycef0550@gmail.com>
2024-08-22 16:27:26 +0100

    final

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,12 @@
+import { ChakraProvider } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {ChakraProvider} from "@chakra-ui/react";
-import App from "./App";
-import theme from "./theme";
-import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
-import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
-import {RouterProvider} from "react-router-dom";
-import Routes from "./Router/Routes";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { RouterProvider } from "react-router-dom";
 import route from "./Router/Routes";
+import theme from "./theme";
 
 const queryClient = new QueryClient();
 
